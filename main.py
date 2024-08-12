6ef0821427228f9ee48957765c0dae5205b6c841
DownhillGFX <125319966+downhillgfx@users.noreply.github.com>
2024-08-12 02:49:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
+## IMPORTS
 import time
 
+
+
+## -----------------------------------------
+
+## Starting + Questions
 print("Starting")
 time.sleep(2)
 user = input("Enter your username: ")
 time.sleep(1)
 password = input("Enter the password: ")
 print("Processing Request")
-if user == "Downhillgfx":
+
+## USER LOGIC
+if user == "Downhillgfx" or user == "downhillgfx":
     print("Username confirmed.")
 else:
-  print("Incorrect Username")
-if user == "test123123":
-  print("Password Confirmed")
+    print("Incorrect Username")
+
+## PASSWORD LOGIC
+if password == "test123123":
+    print("Password Confirmed")
 else:
-  print("Incorrect Password")
+    print("Incorrect Password")
 
 time.sleep(2)
+
+# Loop Space
+for i in range(2):
+    print("-------------------------")
+
+# Granted Access
+if password == "test123123" and (user == "Downhillgfx" or user == "downhillgfx"):
+    print("Granted Access")
+else:
+    print("GET OUT")
